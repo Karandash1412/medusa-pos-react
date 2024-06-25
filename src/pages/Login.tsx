d766630023e3de0ec4eb0681ac6a8062fc1f76c5
Mykola Kardash <bestcole10@gmail.com>
2024-06-24 21:56:12 -0700

    Complete Login page and start Main Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,19 @@
 import "../styles/index.css";
 import "../styles/login.css";
+import { useState } from "react";
 
 const Login = () => {
+    const [visiability, setVisiability] = useState(true);
+
+    function showPaswword() {
+        if (visiability === true) {
+            setVisiability(false);
+        } else {
+            setVisiability(true);
+        }
+    }
     return (
-        <div className="container">
+        <div className="container-login">
             <div className="left-side">
                 <h1>Medusa POS Application</h1>
                 <div className="square"></div>
@@ -12,9 +22,12 @@ const Login = () => {
             <div className="right-side">
                 <div className="login-form">
                     <h2>Login</h2>
-                    <form className="form">
+                    <form className="form" method="">
                         <input type="text" placeholder="www.example.com" required />
-                        <input type="password" placeholder="Password" required />
+                        <div className="password-container">
+                            <input type={visiability ? "password" : "text"} placeholder="Password" required />
+                            <span onClick={showPaswword}><img src="/src/assets/IconButton.png" alt="eye icon" className="icon-toggle-eye" /></span>
+                        </div>
                         <a href="">Forgot your password?</a>
                         <button type="submit">Login</button>
                     </form>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,11 +3,23 @@ import { Link } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
-            <h1>POS Application</h1>
+            <header>
+                <div>
+                    <img src="" alt="" />
+                    <h4>Pavlo Tsyhanok</h4>
+                    <p>Medusa Co.</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </header>
             <nav>
                 <ul>
-                    <li><Link to="/new-order">New Order</Link></li>
-                    <li><Link to="/register-customer">Register Customer</Link></li>
+                    <li><Link to="/orders">Continue Order <span>(Van Designs)</span></Link></li>
+                    <li><Link to="/new-order">Select Customer (new order)</Link></li>
+                    <li><Link to="/register-customer">Register New Customer</Link></li>
+                    <li><Link to="/catalog">Browse Catalog</Link></li>
                     <li><Link to="/settings">Settings</Link></li>
                 </ul>
             </nav>
